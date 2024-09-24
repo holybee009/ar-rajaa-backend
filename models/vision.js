b24f2f1d8f3ec6765fea7f25c515491eff724d76
holybee009 <135528189+holybee009@users.noreply.github.com>
2024-09-24 10:34:27 +0100

    Update vision.js

--- models/vision.js
+++ models/vision.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const visionSchema = new Schema({
-  title: String,
-  content: String,
-  newsPhoto: String,
-  date: Date,
+  vision: String,
+  photo: String,
 });
 
 const VisionPost = mongoose.model("/vision", visionSchema);
